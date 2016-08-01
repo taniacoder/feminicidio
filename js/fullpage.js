be9950ebe317deda273030a5513575a39cd5542b
GenyLeong <genesis.leon.carbajal@gmail.com>
2016-08-01 12:12:08 -0500

    puliendo el resize

--- js/fullpage.js
+++ js/fullpage.js
@@ -13,3 +13,21 @@ $(document).ready(function() {
 			        }				
 			});
 		});
+
+$(".modal-transparent").on('show.bs.modal', function () {
+  setTimeout( function() {
+    $(".modal-backdrop").addClass("modal-backdrop-transparent");
+  }, 0);
+});
+$(".modal-transparent").on('hidden.bs.modal', function () {
+  $(".modal-backdrop").addClass("modal-backdrop-transparent");
+});
+
+$(".modal-fullscreen").on('show.bs.modal', function () {
+  setTimeout( function() {
+    $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+  }, 0);
+});
+$(".modal-fullscreen").on('hidden.bs.modal', function () {
+  $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+});
--- js/main.js
+++ js/main.js
@@ -1,21 +1,34 @@
-$(window).load(function() {
-responsive();
-window.addEventListener('resize', responsive);
 	function responsive() 
 			{
-				var ancho = $(window).outerWidth()
-			    var e=document.getElementsByClassName("row")[1]
-			    if (e.style.display == 'block' )
-			    {
-			        e.style.display = 'none';
-			    }
-			   if (e.style.display = 'none' && ancho > 767)
-			    {
-			        e.style.display = 'block';
-			    }
+				this.ancho = $(window).outerWidth()
+				var r=document.getElementsByClassName("row")[1]
+				if (r && ancho<767){
+					r.remove();				
+				}
 			}
+	function col_xs (ancho) {
+		 var e =document.getElementsByClassName("row")[0]
+		 if (e && ancho<767){
+		 	var col= document.createElement("div");
+		 	col.innerHTML= '<div class="col-xs-6 col-sm-3"><div class="chichico v-center"><i class="fa fa-play-circle-o fa-5x" aria-hidden="true"></i></div></div><div class="col-xs-6 col-sm-3"><div class="chichico v-center"><i class="fa fa-play-circle-o fa-5x" aria-hidden="true"></i></div></div><div class="col-xs-6 col-sm-3"><div class="chichico v-center"><i class="fa fa-play-circle-o fa-5x" aria-hidden="true"></i></div></div>'
+			e.appendChild(col);
+		 }
+
+		var appended = false;
+		if (!appended) {
+			appended = true;
+		}
+$(window).load(function() {
+	$(window).resize(function() {
+responsive();
+col_xs(ancho);
 })
+	})
 
 $(document).on('click', '#ver', function(){
  $.fn.fullpage.moveTo('secondPage', 0);
  });
+
+(function ($) {
+
+	})(jQuery)
