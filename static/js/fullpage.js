9540e79f1d042a342effcf016ab5685c4f706747
GenyLeong <genesis.leon.carbajal@gmail.com>
2016-08-05 13:12:17 -0500

    redes sociales responsive

--- static/js/fullpage.js
+++ static/js/fullpage.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 			$('#fullpage').fullpage({
-				sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-				anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+				sectionsColor: ['#1bbc9b', '#4BBFC3'],
+				anchors: ['testimonios', 'estadisticas'],
 				sectionsColor: ['#3D3D3D', '#FFFFFF'],
 				scrollOverflow: true,
 				scrollOverflowOptions: {
--- static/js/main.js
+++ static/js/main.js
@@ -1,121 +1,118 @@
-	function responsive() 
-			{
-				this.altura = $(window).outerHeight()
-				this.ancho = $(window).outerWidth()
-				var r =document.getElementsByClassName("row")[1]
-				var cf_r= function() {
-					var cf= document.getElementsByClassName("container-fluid")[0]
-					cf.style.height = altura + "px"}
-				if (r && ancho<768){
-					r.style.display = 'none';
-					cf_r()				
-				}
-
-				else{
-					r.style.display = 'block';
-					cf_r()					
-				}			
-			}
+	function responsive() {
+	    this.altura = $(window).outerHeight()
+	    this.ancho = $(window).outerWidth()
+	    var r = document.getElementsByClassName("row")[1]
+	    var cf_r = function() {
+	        var cf = document.getElementsByClassName("container-fluid")[0]
+	        cf.style.height = altura + "px"
+	    }
+	    if (r && ancho < 768) {
+	        r.style.display = 'none';
+	        cf_r()
+	    } else {
+	        r.style.display = 'block';
+	        cf_r()
+	    }
+	}
 
 	$(window).load(function() {
-		responsive();
-		obtener_modal()
-	window.addEventListener('resize', responsive);	
-	poner_modal() 
-		});			
+	    responsive();
+	    obtener_modal()
+	    window.addEventListener('resize', responsive);
+	    poner_modal()
+	});
 
-$(document).on('click', '#ver', function(){
- $.fn.fullpage.moveTo('secondPage', 0);
- });
+	$(document).on('click', '#ver', function() {
+	    $.fn.fullpage.moveTo('estadisticas', 0);
+	});
 
-function poner_modal () {
-	 var chichico= document.getElementsByClassName("chichico")
-	 for (var i=0; i<chichico.length; i++) {
-	 var modal= document.createElement("div");
-	 	modal.innerHTML='<a href= "" type="button" data-toggle="modal" data-target=#'+[i]+'><i class="fa fa-play-circle-o fa-5x" aria-hidden="true"></i></a>'
-	 	chichico[i].appendChild(modal)
-	 }
-}
+	function poner_modal() {
+	    var chichico = document.getElementsByClassName("chichico")
+	    for (var i = 0; i < chichico.length; i++) {
+	        var modal = document.createElement("div");
+	        modal.innerHTML = '<a href= "" type="button" data-toggle="modal" data-target=#' + [i] + '><i class="fa fa-play-circle-o fa-5x" aria-hidden="true"></i></a>'
+	        chichico[i].appendChild(modal)
+	    }
+	}
 
-function obtener_modal(){
-  var modal_transparent = document.querySelectorAll("#modal-transparent")
-  for (var j=0; j<modal_transparent.length; j++){  		
-  modal_transparent[j].setAttribute('id', j);
-  var modal_dialog =  document.createElement("div")
-  modal_dialog.className = "modal-dialog"
-  modal_dialog.innerHTML='<div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">Modal title</h4></div><div class="modal-footer"></div></div>'
-  modal_transparent[j].appendChild(modal_dialog)
-  }
+	function obtener_modal() {
+	    var modal_transparent = document.querySelectorAll("#modal-transparent")
+	    for (var j = 0; j < modal_transparent.length; j++) {
+	        modal_transparent[j].setAttribute('id', j);
+	        var modal_dialog = document.createElement("div")
+	        modal_dialog.className = "modal-dialog"
+	        modal_dialog.innerHTML = '<div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">Modal title</h4></div><div class="modal-footer"></div></div>'
+	        modal_transparent[j].appendChild(modal_dialog)
+	    }
 
-var obtener_md= document.getElementsByClassName("modal-content")
-for (var k =0; k<obtener_md.length; k++){
-	  switch(obtener_md[k]) {
-	    case obtener_md[0]:
-  		var modal_body= document.createElement("div")
-  		modal_body.className ="modal-body"
-		modal_body.innerHTML= '<iframe width="560" height="315" src="https://www.youtube.com/embed/tdM_ArIOkl8" frameborder="0" allowfullscreen></iframe><div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus iure est, ab sequi, similique dolores eius totam velit quia illo, porro vitae quaerat magnam repellat eos laborum amet molestiae natus neque repudiandae dignissimos. Maiores eius ullam ducimus corporis hic ea, quod in, aspernatur cupiditate et mollitia quasi excepturi deserunt. Commodi!</div>'
-	      obtener_md[0].appendChild(modal_body)	      	            
-	      break;
-	    case obtener_md[1]:
-	    var modal_body= document.createElement("div")
-  		modal_body.className ="modal-body"
-		modal_body.innerHTML= '<iframe width="100%" height="315" src="https://www.youtube.com/embed/ogeOcqnSKQQ" frameborder="0" allowfullscreen></iframe>'
-	      obtener_md[1].appendChild(modal_body)	
-	      break;
-	    case obtener_md[2]:
-	    var modal_body= document.createElement("div")
-  		modal_body.className ="modal-body"
-		modal_body.innerHTML= '<iframe width="100%" height="315" src="https://www.youtube.com/embed/ogeOcqnSKQQ" frameborder="0" allowfullscreen></iframe>'
-	      obtener_md[2].appendChild(modal_body)	
-	      break;
-	    case obtener_md[3]:
-	    var modal_body= document.createElement("div")
-  		modal_body.className ="modal-body"
-		modal_body.innerHTML= '<iframe width="100%" height="315" src="https://www.youtube.com/embed/ogeOcqnSKQQ" frameborder="0" allowfullscreen></iframe>'
-	      obtener_md[3].appendChild(modal_body)	
-	      break;
-	    case obtener_md[4]:
-	    var modal_body= document.createElement("div")
-  		modal_body.className ="modal-body"
-		modal_body.innerHTML= '<iframe width="100%" height="315" src="https://www.youtube.com/embed/ogeOcqnSKQQ" frameborder="0" allowfullscreen></iframe>'
-	      obtener_md[4].appendChild(modal_body)	
-	      break;
-	    case obtener_md[5]:
-	    var modal_body= document.createElement("div")
-  		modal_body.className ="modal-body"
-		modal_body.innerHTML= '<iframe width="100%" height="315" src="https://www.youtube.com/embed/ogeOcqnSKQQ" frameborder="0" allowfullscreen></iframe>'
-	      obtener_md[5].appendChild(modal_body)	
-	      break;
-	    case obtener_md[6]:
-	    var modal_body= document.createElement("div")
-  		modal_body.className ="modal-body"
-		modal_body.innerHTML= '<iframe width="100%" height="315" src="https://www.youtube.com/embed/ogeOcqnSKQQ" frameborder="0" allowfullscreen></iframe>'
-	      obtener_md[6].appendChild(modal_body)	
-	      break;
-	    case obtener_md[7]:
-	    var modal_body= document.createElement("div")
-  		modal_body.className ="modal-body"
-		modal_body.innerHTML= '<iframe width="100%" height="315" src="https://www.youtube.com/embed/ogeOcqnSKQQ" frameborder="0" allowfullscreen></iframe>'
-	      obtener_md[7].appendChild(modal_body)	
-	      break;
-	    case obtener_md[8]:
-	    var modal_body= document.createElement("div")
-  		modal_body.className ="modal-body"
-		modal_body.innerHTML= '<iframe width="100%" height="315" src="https://www.youtube.com/embed/ogeOcqnSKQQ" frameborder="0" allowfullscreen></iframe>'
-	      obtener_md[8].appendChild(modal_body)	
-	      break;	      
-	  }
-	  obtener_md[k].insertBefore(modal_body, obtener_md[k].childNodes[1]);	
-}
-footer_modal()
-  }
+	    var obtener_md = document.getElementsByClassName("modal-content")
+	    for (var k = 0; k < obtener_md.length; k++) {
+	        switch (obtener_md[k]) {
+	            case obtener_md[0]:
+	                var modal_body = document.createElement("div")
+	                modal_body.className = "modal-body"
+	                modal_body.innerHTML = '<iframe width="100%" height="315" src="https://www.youtube.com/embed/tdM_ArIOkl8" frameborder="0" allowfullscreen></iframe><div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus iure est, ab sequi, similique dolores eius totam velit quia illo, porro vitae quaerat magnam repellat eos laborum amet molestiae natus neque repudiandae dignissimos. Maiores eius ullam ducimus corporis hic ea, quod in, aspernatur cupiditate et mollitia quasi excepturi deserunt. Commodi!</div>'
+	                obtener_md[0].appendChild(modal_body)
+	                break;
+	            case obtener_md[1]:
+	                var modal_body = document.createElement("div")
+	                modal_body.className = "modal-body"
+	                modal_body.innerHTML = '<iframe width="100%" height="315" src="https://www.youtube.com/embed/ogeOcqnSKQQ" frameborder="0" allowfullscreen></iframe>'
+	                obtener_md[1].appendChild(modal_body)
+	                break;
+	            case obtener_md[2]:
+	                var modal_body = document.createElement("div")
+	                modal_body.className = "modal-body"
+	                modal_body.innerHTML = '<iframe width="100%" height="315" src="https://www.youtube.com/embed/ogeOcqnSKQQ" frameborder="0" allowfullscreen></iframe>'
+	                obtener_md[2].appendChild(modal_body)
+	                break;
+	            case obtener_md[3]:
+	                var modal_body = document.createElement("div")
+	                modal_body.className = "modal-body"
+	                modal_body.innerHTML = '<iframe width="100%" height="315" src="https://www.youtube.com/embed/ogeOcqnSKQQ" frameborder="0" allowfullscreen></iframe>'
+	                obtener_md[3].appendChild(modal_body)
+	                break;
+	            case obtener_md[4]:
+	                var modal_body = document.createElement("div")
+	                modal_body.className = "modal-body"
+	                modal_body.innerHTML = '<iframe width="100%" height="315" src="https://www.youtube.com/embed/ogeOcqnSKQQ" frameborder="0" allowfullscreen></iframe>'
+	                obtener_md[4].appendChild(modal_body)
+	                break;
+	            case obtener_md[5]:
+	                var modal_body = document.createElement("div")
+	                modal_body.className = "modal-body"
+	                modal_body.innerHTML = '<iframe width="100%" height="315" src="https://www.youtube.com/embed/ogeOcqnSKQQ" frameborder="0" allowfullscreen></iframe>'
+	                obtener_md[5].appendChild(modal_body)
+	                break;
+	            case obtener_md[6]:
+	                var modal_body = document.createElement("div")
+	                modal_body.className = "modal-body"
+	                modal_body.innerHTML = '<iframe width="100%" height="315" src="https://www.youtube.com/embed/ogeOcqnSKQQ" frameborder="0" allowfullscreen></iframe>'
+	                obtener_md[6].appendChild(modal_body)
+	                break;
+	            case obtener_md[7]:
+	                var modal_body = document.createElement("div")
+	                modal_body.className = "modal-body"
+	                modal_body.innerHTML = '<iframe width="100%" height="315" src="https://www.youtube.com/embed/ogeOcqnSKQQ" frameborder="0" allowfullscreen></iframe>'
+	                obtener_md[7].appendChild(modal_body)
+	                break;
+	            case obtener_md[8]:
+	                var modal_body = document.createElement("div")
+	                modal_body.className = "modal-body"
+	                modal_body.innerHTML = '<iframe width="100%" height="315" src="https://www.youtube.com/embed/ogeOcqnSKQQ" frameborder="0" allowfullscreen></iframe>'
+	                obtener_md[8].appendChild(modal_body)
+	                break;
+	        }
+	        obtener_md[k].insertBefore(modal_body, obtener_md[k].childNodes[1]);
+	    }
+	    footer_modal()
+	}
 
-function footer_modal(){
-	  var poner_redes = document.getElementsByClassName("modal-footer")
-	  for (z=0; z<poner_redes.length; z++){	  	
-  		var redes = document.createElement("div")
-  	redes.innerHTML = '<div class="redes-sociales"><!--Twitter--><a href="https://twitter.com/share" class="twitter-share-button">Tweet</a> <!--Twitter--><!--facebook--><div class="fb-like" data-href="https://taniacoder.github.io/feminicidio/static/#firstPage" data-layout="standard" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div><!--facebook--></div>'
-  	poner_redes[z].appendChild(redes)
-	  }
-}
-
+	function footer_modal() {
+	    var poner_redes = document.getElementsByClassName("modal-footer")
+	    for (z = 0; z < poner_redes.length; z++) {
+	        var redes = document.createElement("div")
+	        redes.innerHTML = '<div class="redes-sociales"><!--Twitter--><a href="https://twitter.com/share" class="twitter-share-button">Tweet</a> <!--Twitter--><!--facebook--><div class="fb-like" data-href="https://taniacoder.github.io/feminicidio/static/#firstPage" data-layout="standard" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div><!--facebook--></div>'
+	        poner_redes[z].appendChild(redes)
+	    }
+	}
